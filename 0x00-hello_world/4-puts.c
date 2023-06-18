869f30577ca926c7217c10f4b76175dae6810a0c
Mo-helmyy <modyrf1@gmail.com>
2023-06-18 16:27:18 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: A C program that prints with put function
  *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle") :
+	return (0) :
 }
