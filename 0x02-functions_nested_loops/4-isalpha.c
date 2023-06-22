3a15f983694c7883fec74aa2f842eaf8b9a8d76f
Mo-helmyy <modyrf1@gmail.com>
2023-06-21 18:17:09 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,11 +9,11 @@
  * Return: 1 is C if true else 0
 */
 
-int _isalpha(int C)
+int _isalpha(int C);
 {
 	if (C >= 97 && C <= 122)
 		return (1);
-	if (C >= 65 && c <= 90)
+	if (C >= 65 && C <= 90)
 		return (1);
 	return (0);
 }
